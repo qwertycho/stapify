d0e54eaa624ed3e2d856561562a8eab865a4ce4d
qwertycho <tychovopstal@gmail.com>
2023-02-23 17:44:02 +0100

    timer, leds, com en docs

--- iot/main/Communicatie.h
+++ iot/main/Communicatie.h
@@ -6,92 +6,156 @@
 #include "Buffer.h"
 #include <ArduinoBLE.h>
 
-
-// COmmunicatie is een interface voor de logger class.
-// Communicatie is een singleton class, zodat er maar 1 instance van is.
-// Communicatie is een child class van Logger, zodat de functie sendMessage() geimplementeerd moet worden.
-class Communicatie : public Logger{
+/**
+ * @brief De Communicatie class is een singleton class die de communicatie met de buitenwereld regelt.
+ * Krijg een nieuwe instance van de class met de functie getInstance().
+ * @details De class heeft een buffer die de berichten opslaat en een functie om berichten te versturen.
+ */
+class Communicatie : public Logger
+{
     // de instellingen voor de communicatie
-    private:
-        BLEService service = "19B10000-E8F2-537E-4F6C-D104768A1214";
-        String ssid = "stapifi";
-        String pass = "stapifi";
-        int serialSpeed = 9600;
-        ComBuffer buffer; 
+private:
+    BLEService service = "19B10000-E8F2-537E-4F6C-D104768A1214";
+    String ssid = "stapifi";
+    String pass = "stapifi";
+    int serialSpeed = 9600;
+    ComBuffer buffer;
 
-// de pointer naar de enige instance van de class, deze word gedeelt als de functie getInstance() wordt aangeroepen
-        static Communicatie* instancePTR;
-        Communicatie(){
-            buffer = ComBuffer();
+    // de pointer naar de enige instance van de class, deze word gedeelt als de functie getInstance() wordt aangeroepen
+    static Communicatie *instancePTR;
+    Communicatie()
+    {
+        buffer = ComBuffer();
+    }
+
+public:
+    // Het contructor gedeelte van de singleton pattern
+
+    // de destructor
+    Communicatie(const Communicatie &obj) = delete;
+
+    /**
+     * @brief De functie om de instance van de class te krijgen.
+     * @return Communicatie* de instance van de class
+     */
+    static Communicatie *getInstance()
+    {
+        if (instancePTR == nullptr)
+        {
+            instancePTR = new Communicatie();
         }
+        return instancePTR;
+    }
 
-    public:
-
-        // Het contructor gedeelte van de singleton pattern
-
-        // de destructor
-        Communicatie(const Communicatie &obj) = delete;
-
-        // de functie om de instance te krijgen
-        static Communicatie* getInstance(){
-            if(instancePTR == nullptr){
-                instancePTR = new Communicatie();
-            }
-            return instancePTR;
+    /**
+     * @brief De functie om de serial verbinding te starten.
+     * @details Controleerd eerst of er al een verbinding is.
+     */
+    void enableSerial()
+    {
+        if (!Serial)
+        {
+            Serial.begin(serialSpeed);
+            log(1, "Serial connected with speed: " + serialSpeed);
         }
+        else
+        {
+            log(2, "Serial already connected with speed: " + serialSpeed);
+        }
+    }
 
-        // de functies
+    /**
+     * @brief De functie om de BLE verbinding te starten.
+     * @details Controleerd eerst of er al een verbinding is.
+     */
+    void enableBLE()
+    {
+        if (!BLE.begin())
+        {
+            log(3, "BLE failed to start");
+        }
+        else
+        {
+            // voor wat voor reden dan ook wilt setLocalName() een char* hebben en geen String
+            BLE.setLocalName("Staptor");
+            BLE.setAdvertisedService(service);
+            BLE.addService(service);
+            BLE.advertise();
+            log(1, "BLE started");
+        }
+    }
 
-        // de functie om een serial verbinding te maken
-        // de functie moet controleren of er al een verbinding is zodat er geen fouten ontstaan
-        void enableSerial(){
-            if(!Serial){
-                Serial.begin(serialSpeed);
-                log(1, "Serial connected with speed: " + serialSpeed);
-            }else{
-                log(2, "Serial already connected with speed: " + serialSpeed);
-            }
-        }
+    /**
+     * @brief Suurt het verbonden device terug
+     * @details Deze functie is nog niet af.
+     * @todo Deze functie moet nog worden afgemaakt.
+     * @return BLEDevice het verbonden device
+     */
+    void getDevice()
+    {
+        BLEDevice tellie = BLE.central();
+    }
 
-    // start een bluetooth verbinding
-    // controleerd eerst of er al een verbinding is
-    // voor bluetooth is de library nodig: arduinoBLE
-        void enableBLE(){
-            if(!BLE.begin()){
-                log(3, "BLE failed to start");
-            }else{
-                // voor wat voor reden dan ook wilt setLocalName() een char* hebben en geen String
-                BLE.setLocalName("Staptor");
-                BLE.setAdvertisedService(service);
-                BLE.addService(service);
-                BLE.advertise();
-                log(1, "BLE started");
-            }
-        }
-        
-        void getDevice(){
-            BLEDevice tellie = BLE.central();
-        }
+    /**
+     * @brief Interne functie voor het checken of een bericht even lang is.
+     * @param String bericht
+     * @return bool true als het bericht even lang is
+     */
+    bool isEven(String &message)
+    {
+        bool isEven = (message.length() % 2 == 0);
+        return isEven;
+    }
 
-        bool isEven(String& message){
-            bool isEven = (message.length() % 2 == 0);
-            return isEven;
-        }
-        
-        void sendMessage(String type ,String message){
-            String msgType = "[" + type + "] ";
-            Serial.println(msgType + message + " ," + isEven(message) + ";");
-        }
+    /**
+     * @brief De functie om een bericht te versturen.
+     * @details Deze functie stuurt een bericht naar de serial verbinding.
+     * @param String type het type bericht
+     * @param String message het bericht
+     */
+    void sendMessage(String type, String message)
+    {
+        String msgType = "[" + type + "] ";
+        Serial.println(msgType + message + " ," + isEven(message) + ";");
+    }
 
-    // dit is een functie die moet worden geimplementeerd omdat het een pure virtual functie is in de class Logger
-    // oftewel: deze functie maakt Logger werkend.
-        void sendLog(String message){
-            bool isEven = (message.length() % 2 == 0);
-            Serial.println("[LOG] " + message + " ," + isEven + ";");
+    /**
+     * @brief De functie om een log te versturen.
+     * @details Deze functie stuurt een bericht naar de serial verbinding.
+     * @param String message het bericht
+     */
+    void sendLog(String message)
+    {
+        bool isEven = (message.length() % 2 == 0);
+        Serial.println("[LOG] " + message + " ," + isEven + ";");
+    }
+
+    /**
+     * @brief test functie om data in de buffer op te slaan 
+     * @details Deze functie slaat message op in de buffer
+     * @param String message het bericht
+     */
+    void addBuffer(String message)
+    {
+        buffer.saveData(message);
+    }
+
+    /**
+     * @brief Stuur alle data in de buffer naar de serial verbinding
+     * @details Deze functie stuurt de data in de buffer naar de serial verbinding
+     * @return bool true als het is gelukt
+     */
+    bool sendBuffer()
+    {
+        for (int i = 0; i < buffer.getLength(); i++)
+        {
+            Serial.println(buffer.getData()[i]);
         }
+        return true;
+    }
 };
 
 // de initialisatie van de pointer naar de instance, maakt ding null zodat de functie getInstance() weet dat er nog geen instance is.
-Communicatie* Communicatie::instancePTR = nullptr;
+Communicatie *Communicatie::instancePTR = nullptr;
 
 #endif
--- iot/main/logger.h
+++ iot/main/logger.h
@@ -1,51 +1,61 @@
 
 #ifndef LOG_h
 #define LOG_h
-
+#include <Arduino.h>
+#include "leds.h"
 // de class Logger is er om alleen bepaalde informatie te loggen en om als enkel punt te dienen voor het loggen van informatie
-class Logger {
-  private:
-    u_int8_t logLevel = 0;
+class Logger
+{
+private:
+  u_int8_t logLevel = 0;
 
-// Maakt een string met de juiste informatie voor de log
-    String getInfoMessage(u_int8_t logLevel, String message) {
-      String infoMessage = "";
-      switch (logLevel) {
-        case 1:
-          infoMessage = "INFO: ";
-          break;
-        case 2:
-          infoMessage = "WARNING: ";
-          break;
-        case 3:
-          infoMessage = "ERROR: ";
-          break;
-        default:
-          infoMessage = "UNKNOWN: ";
-          break;
-      }
-      infoMessage += message;
-      return infoMessage;
+  // Maakt een string met de juiste informatie voor de log
+  String getInfoMessage(u_int8_t logLevel, String message)
+  {
+    String infoMessage = "";
+    switch (logLevel)
+    {
+    case 1:
+      infoMessage = "INFO: ";
+      ledOn(1);
+      break;
+    case 2:
+      infoMessage = "WARNING: ";
+      ledOn(2);
+      break;
+    case 3:
+      infoMessage = "ERROR: ";
+      ledOn(3);
+      break;
+    default:
+      infoMessage = "UNKNOWN: ";
+      break;
     }
+    infoMessage += message;
+    return infoMessage;
+  }
 
-  public:
-
-    void setLevel(u_int8_t logLevel) {
-      this->logLevel = logLevel;
-    }
+public:
+  void setLevel(u_int8_t logLevel)
+  {
+    this->logLevel = logLevel;
+  }
 
   virtual void sendLog(String message) = 0;
 
-// Ontvangt een logniveau en een bericht
-// Als het logniveau hoger is dan het logniveau van de logger wordt het bericht geprint
-    void log(u_int8_t logLevel, String message) {
-      if (logLevel >= this->logLevel) {
-        if(Serial){
-          // Serial.println(getInfoMessage(logLevel, message));
-          sendLog(getInfoMessage(logLevel, message));
-        }
+  // Ontvangt een logniveau en een bericht
+  // Als het logniveau hoger is dan het logniveau van de logger wordt het bericht geprint
+  void log(u_int8_t logLevel, String message)
+  {
+    if (logLevel >= this->logLevel)
+    {
+      if (Serial)
+      {
+        // Serial.println(getInfoMessage(logLevel, message));
+        sendLog(getInfoMessage(logLevel, message));
       }
-      }
-}; 
+    }
+  }
+};
 
 #endif
