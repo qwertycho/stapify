80db23f54ebe401c6a8fdc5f4379dccd1989d552
Peter <101114264+JustPeter64@users.noreply.github.com>
2023-03-16 15:23:15 +0100

    inlog aangemaakt en packageproblemen opgelost

---
+++ stapify/Inlog.js
@@ -0,0 +1,9 @@
+// import React { useState } from "react";
+// import { Button, Text, TextInput, View, StyleSheet, Alert } from "react-native";
+
+// const Inlog = ({ navigation }) => {
+
+//     const [formUsername, setFormUsername] = useState("Enter your Username");
+//     const [formPassword, setFormPassword] = useState("Enter your password");
+
+// }
