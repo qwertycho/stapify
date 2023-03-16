57d3d9eb6055c8852ad603441f8c71d094e925ff
Peter <101114264+JustPeter64@users.noreply.github.com>
2023-03-16 15:36:27 +0100

    Update Inlog.js

--- stapify/Inlog.js
+++ stapify/Inlog.js
@@ -1,9 +1,1 @@
-// import React { useState } from "react";
-// import { Button, Text, TextInput, View, StyleSheet, Alert } from "react-native";
 
-// const Inlog = ({ navigation }) => {
-
-//     const [formUsername, setFormUsername] = useState("Enter your Username");
-//     const [formPassword, setFormPassword] = useState("Enter your password");
-
-// }
