f607d178780073ef635141dfef6bc6b1c1b68b05
qwertycho <tychovopstal@gmail.com>
2023-03-28 13:48:20 +0200

    api inlog token en accounts update

--- website/models/Root.js
+++ website/models/Root.js
@@ -5,18 +5,25 @@ const AccountModel = require("../models/Account");
 const pool = require("./Database");
 const Account = new AccountModel(pool);
 
+const express = require("express");
+const app = express();
+
 var root = {
   accounts: async () => {
     let accounts = await Account.getAccounts();
     console.log(accounts);
-    return accounts[0].username;
+    return accounts;
   },
   account: async ({ username }) => {
 
     return await Account.getAccount(username);
   },
     login: async ({ username, password }) => {
-        return await Account.login(username, password);
+        if(await Account.login(username, password)){
+          return await Account.createCookie(username);
+        } else {
+          return false;
+        }
     },
     createAccount: async ({ username, password, geboortedatum }) => {
         return await Account.createAccount(username, password, geboortedatum);
