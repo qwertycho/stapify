0bf1a4029a5770d069c3bd6f3d7cf4ec4050dd38
RemcoHottentot <87954@glr.nl>
2023-05-16 14:34:54 +0200

    achterstand

--- iot/raspberrypi/heart.py
+++ iot/raspberrypi/heart.py
@@ -1,7 +1,9 @@
 # heartbeat sensor werkt met: https://www.adafruit.com/product/1073
 
 import RPi.GPIO as GPIO
+import json
 import time
+from graphqlclient import GraphQLClient
 
 # Set up the GPIO pins
 GPIO.setmode(GPIO.BCM)
@@ -11,6 +13,57 @@ GPIO.setup(PulseSensorPin, GPIO.IN)
 # Set up the PulseSensor
 Threshold = 550
 Signal = 0
+
+# graphgl client
+client = GraphQLClient('https://schoolmoettestdomeinenhebben.nl/graphql')
+
+# login query
+login = '''
+query Login($username: String!, $password: String!) {
+    login(username: $username, password: $password)
+}
+'''
+
+# login gegevens
+variablesLogin = {
+    'username': 'azertycho',
+    'password': '123',
+}
+
+# login antwoord
+response_login = client.execute(login, variablesLogin)
+# get the cookie from the response which is located in data>login>cookie
+cookie = json.loads(response_login)['data']['login']
+
+
+
+
+
+
+
+# stap toevoegen query
+voegHartslagToe = '''
+mutation AddStap($aantalStappen: Int!, $cookie: String!) {
+    stappen(aantalStappen: $aantalStappen, cookie: $cookie) {
+        code
+        message
+    }
+}
+'''
+
+# stap toevoegen variabelen
+variablesStap = {
+    'aantalStappen': 1,
+    'cookie': cookie
+}
+
+
+
+
+
+
+
+
 
 # Main loop
 while True:
@@ -23,4 +76,6 @@ while True:
     # If the signal is above the threshold, print a message
     if Signal > Threshold:
         print("Heartbeat detected!")
-    
+
+        # stuur Signal naar graphql
+        
